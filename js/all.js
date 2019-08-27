36218225e7483c1f1d526901cffa2d6774aeaa88
hiddenski <loonysnow@gmail.com>
2019-08-27 17:37:59 +0500

    update

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
 $(function() {
-    
+    $('.content table').removeAttr('class border').addClass('uk-table uk-table-divider uk-table-middle');
+    $('.content table').wrap("<div class='uk-overflow-auto uk-margin-medium'></div>");
 });
